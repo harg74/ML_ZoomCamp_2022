bb773ad4cd79cf270512990484b5cce666645c5c
Humberto Rodriguez <harg_123@hotmail.com>
2022-10-03 12:05:55 -0500

    5.4 Serving the Churning Model

--- 5-Deploying_Machine_Learning_Models/predict.py
+++ 5-Deploying_Machine_Learning_Models/predict.py
@@ -1,35 +1,32 @@
 import pickle
+from flask import request, Flask, jsonify
+
 
 with open('output_file', 'rb') as f_in:
     dv, model = pickle.load(f_in)
 
-customer = {
-            'gender': 'female',
-            'seniorcitizen': 0,
-            'partner': 'yes',
-            'dependents': 'no',
-            'phoneservice': 'no',
-            'multiplelines': 'no',
-            'internetservice': 'dsl',
-            'onlinesecurity': 'no',
-            'onlinebackup': 'yes',
-            'deviceprotection': 'no',
-            'techsupport': 'no',
-            'streamingtv': 'no',
-            'streamingmovies': 'no',
-            'contract': 'month-to-month',
-            'paperlessbilling': 'yes',
-            'paymentmethod': 'electronic_check',
-            'tenure': 1,
-            'monthlycharges': 29.85,
-            'totalcharges': 29.85,
-        }
+app=Flask('churn')
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    #get_json: tell flask we are getting json and extract the body of request as json
+    #requests: will return the body of the request as python dictionary
+    #it will take the body of the request, assume that is json, parse it and return a python dict.
+    customer = request.get_json()
+
+    #DictVectorizer expects a list of dictionaries
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0,1]
+    churn = y_pred >= 0.5
+
+    result = {
+        'churn_probability': float(y_pred), #np float into python float
+        'churn': bool(churn) #np boolean into python boolean
+    }
+
+    #we return a dictionary that will be converted into a json with jsonify
+    return jsonify(result)
 
 
-#DictVectorizer expects a list of dictionaries
-X = dv.transform([customer])
-
-y_pred = model.predict_proba(X)[0,1]
-
-print('input', customer)
-print('churn probability', y_pred)
+if __name__ == "__main__":
+    app.run(debug=True)
