dd1a7144187ee3ed64b64133d917cee540c31b57
Humberto Rodriguez <harg_123@hotmail.com>
2022-10-03 21:41:38 -0500

    5.6 Enviroment Management

--- 5-Deploying_Machine_Learning_Models/predict.py
+++ 5-Deploying_Machine_Learning_Models/predict.py
@@ -30,3 +30,4 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    #app.run(debug=True, host='0.0.0.0', port=9696)
---
+++ 5-Deploying_Machine_Learning_Models/test_send_cust.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+
+#url = 'http://0.0.0.0:9696/predict'
+url = 'http://127.0.0.1:5000/predict'
+
+
+customer_id = 'xyz-123'
+
+customer = {
+    "gender": "female",
+    "seniorcitizen": 0,
+    "partner": "yes",
+    "dependents": "no",
+    "phoneservice": "no",
+    "multiplelines": "no",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "onlinebackup": "yes",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 10,
+    "monthlycharges": 29.85,
+    "totalcharges": 29.85,
+}
+
+response = requests.post(url, json=customer).json()
+print(response)
+
+if response['churn'] == True:
+    print(f'send promo email to: {customer_id}')
+else:
+    print(f'not sending promo email to: {customer_id}')
