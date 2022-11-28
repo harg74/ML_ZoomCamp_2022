6c057acb0e1e65af274352e3b90e382b49198e2e
Humberto Rodriguez <harg_123@hotmail.com>
2022-11-27 20:59:53 -0600

    9.10 Module dino-dragon

---
+++ 9-Serverless_Deep_Learning/homework/lambda_function.py
@@ -0,0 +1,56 @@
+import tflite_runtime.interpreter as tflite
+import numpy as np
+from io import BytesIO
+from urllib import request
+from PIL import Image
+import os
+
+MODEL_NAME = os.getenv('MODEL_NAME', 'dino_dragon.tflite')
+
+interpreter = tflite.Interpreter(model_path=MODEL_NAME)
+
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+
+output_index = interpreter.get_output_details()[0]['index']
+
+def download_image(url):
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    return img
+
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+
+def prepare_input(x):
+    return x / 255.0
+
+def predict(url):
+    img=download_image(url)
+    img=prepare_image(img, target_size=(150,150))
+
+    x = np.array(img, dtype='float32')
+    X = np.array([x])
+    X = prepare_input(X)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+
+    preds = interpreter.get_tensor(output_index)
+    
+    return float(preds[0, 0])
+
+def lambda_handler(event, context):
+    url = event['url']
+    pred = predict(url)
+    result = {
+        'prediction': pred
+    }
+
+    return result 
--- 9-Serverless_Deep_Learning/test.py
+++ 9-Serverless_Deep_Learning/test.py
@@ -1,6 +1,8 @@
 import requests
 
-url='http://localhost:8080/2015-03-31/functions/function/invocations'
+#url='http://localhost:8080/2015-03-31/functions/function/invocations'
+
+url='https://2q2p4xuz0k.execute-api.us-east-1.amazonaws.com/test/predict'
 
 data = {'url':'http://bit.ly/mlbookcamp-pants'}
 
