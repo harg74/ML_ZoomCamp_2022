ef69943354f0abc65bc983b8e1f88594c9d9ff75
Humberto Rodriguez <harg_123@hotmail.com>
2022-12-07 22:07:08 -0600

    update yaml files and test.py file

--- 10-Kubernetes_and_TensorFlow_Serving/test.py
+++ 10-Kubernetes_and_TensorFlow_Serving/test.py
@@ -1,7 +1,8 @@
 import requests
 
 #url='http://localhost:9696/predict'
-url='http://localhost:8080/predict'
+#url='http://localhost:8080/predict'
+url='http://af85d63246e44456a81c31d019d01cb1-2132056441.us-east-1.elb.amazonaws.com/predict'
 
 data = {'url':'http://bit.ly/mlbookcamp-pants'}
 
